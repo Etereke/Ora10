12ea0c5b50d05bd427c01cb7275f583196f893b5
Etereke <zsugya.martin@gmail.com>
2022-11-17 09:34:07 +0100

    Add project files.

---
+++ Ora10/Crime.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Ora10
+{
+    public class Crime
+    {
+        public string Type;
+        public int Year;
+        public int Month;
+        public int Day;
+        public int Hour;
+        public int Minute;
+        public string HundredBlock;
+        public string Neighbourhood;
+        public float X;
+        public float Y;
+        public float Latitude;
+        public float Longitude;
+
+
+        public Crime() { }
+        [JsonConstructor]
+        public Crime(string type, int year, int month, int day, int hour, int minute,
+            string hundred_Block, string neighbourhood, float x, float y, float latitude,
+            float longitude)
+        {
+            Type = type;
+            Year = year;
+            Month = month;
+            Day = day;
+            Hour = hour;
+            Minute = minute;
+            HundredBlock = hundred_Block;
+            Neighbourhood = neighbourhood;
+            X = x;
+            Y = y;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+        //[JsonConstructor]
+        //public Crime() { }
+
+        public override string ToString()
+        {
+            return $"Type: {Type}\n" +
+                   $"Year: {Year}\n" +
+                   $"Month: {Month}\n" +
+                   $"Day: {Type}\n" +
+                   $"Hour: {Type}\n" +
+                   $"Minute: {Type}\n" +
+                   $"HundredBlock: {HundredBlock}\n" +
+                   $"Neighbourhood: {Neighbourhood}\n" +
+                   $"X: {X}\n" +
+                   $"Y: {Y}\n" +
+                   $"Latitude: {Latitude}\n" +
+                   $"Longitude: {Longitude}\n\n";
+        }
+    }
+    public class CrimeList
+    {
+        public List<Crime> Crimes { get; set; }
+    }
+}
---
+++ Ora10/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using System.Text.Json;
+using Ora10;
+var json = new WebClient().DownloadString("https://math.uni-pannon.hu/~lipovitsa/cs/data/crime.json");
+//Console.WriteLine(json);
+var Crimes = JsonSerializer.Deserialize<List<Object>>(json);
+List<Crime> CrimesList = new List<Crime>();
+foreach(var crime in Crimes)
+{
+    Console.WriteLine(crime);
+}
